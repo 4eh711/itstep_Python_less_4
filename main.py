d3301682d058ca2cd9b53cb65f57dc37b8d384ec
4eh711 <Cheh711@gmail.com>
2023-08-27 16:46:17 +0300

    Less_4 5-th commit (метод Super_ex.1)

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-class Hello_world:
-    hello="Hello"
-    _hello="_Hello"
-    __hello="__Hello"
+class Hello:
     def __init__(self):
-        self.world="World"
-        self._world = "_World"
-        self.__world = "__World"
-    def printer(self):
-        print(self.hello)
-        print(self._hello)
-        print(self.__hello)
-        print(self.world)
-        print(self._world)
-        print(self.__world)
+        print("Hello!")
 
-class Hi(Hello_world):
-    def hi_print(self):
-        print(self.hello)
-        print(self.world)
-        print(self._hello)
-        print(self._world)
-        print(self.__hello)
-        print(self.__world)
+class Hello_World(Hello):
+    def __init__(self):
+        super().__init__()
+        print("World!")
 
-hello=Hello_world()
-hello.printer()
-hi=Hi()
-hi.hi_print()
+hello_world=Hello_World()
