71d7b729ad1c3ed6566adcd6e67fbd40045363bb
4eh711 <Cheh711@gmail.com>
2023-08-27 17:35:38 +0300

    Less_4 11-th commit (Передавання аргумекнтів до класів ex.3)

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+# А якщо в конструктор класу треба передати вхідні
+# параметри? Впоратись із цим допоможе запаковування
+# параметрів. Самі ж *args та **kwargs треба передати
+# як аргумент базового класу
 class Computer:
-    def __init__(self):
-        super().__init__()
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model=model
         self.memory=128
 
 class Display:
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.resolution="4k"
 
 class SmartPhone(Display, Computer):
     def print_info(self):
+        print(self.model)
         print(self.resolution)
         print(self.memory)
 
-iphone = SmartPhone()
+iphone = SmartPhone(model="Last")
 iphone.print_info()
