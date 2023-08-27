73a890f58f2c50705c9df981ae632c7d41cf13d7
4eh711 <Cheh711@gmail.com>
2023-08-27 16:23:11 +0300

    Less_4 3-st commit (успадкування в 3-х класах)

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-class Human:
-    height = 170
-    satiety=50
-
-class Student(Human):
-     satiety=0
-class Worker(Human):
+class Grandparent:
+    height=170
     satiety=100
+    age=60
 
-nick=Student()
-ann=Worker()
-print(nick.satiety)
-print(ann.satiety)
+class Parent(Grandparent):
+    age=40
+
+class Child(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+
+nick = Child()
